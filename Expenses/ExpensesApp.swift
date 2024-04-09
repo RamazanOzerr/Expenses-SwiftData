81f9bf830e76eb0d88bd661d7ac861c16d630906
Ramazan Ozer <ozerramazan991@gmail.com>
2024-04-09 20:37:31 +0300

    crud operations done

--- Expenses/ExpensesApp.swift
+++ Expenses/ExpensesApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct ExpensesApp: App {
@@ -13,5 +14,9 @@ struct ExpensesApp: App {
         WindowGroup {
             ContentView()
         }
+        
+        .modelContainer(for: [Expense.self])
     }
+    
+    
 }
--- Expenses/Model/Expense.swift
+++ Expenses/Model/Expense.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Expense{
+    
+    var name: String
+    var date: Date
+    var value: Double
+    
+    init(name: String, date: Date, value: Double) {
+        self.name = name
+        self.date = date
+        self.value = value
+    }
+    
+}
+
