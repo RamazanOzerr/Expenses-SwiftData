9450beae1d941039ed12f532b3a8bb304f579814
Ramazan Ozer <ozerramazan991@gmail.com>
2024-04-09 18:45:01 +0300

    Initial Commit

---
+++ Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by Ramazan Ozer on 9.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
